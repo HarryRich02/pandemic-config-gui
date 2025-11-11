feda6b7ced8d5cf778a39b4e7c4641b88d6d4841
HarryRich02 <klvr36@durham.ac.uk>
2025-11-11 12:16:22 +0000

    POC rewritten with Qt

--- src/app.py
+++ src/app.py
@@ -1,17 +1,7 @@
 import os, sys
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QWidget,
-    QHBoxLayout,
-    QVBoxLayout,
-    QSplitter,
-    QLabel,
-    QLineEdit,
-    QComboBox,
-    QPushButton,
-)
+from PyQt5 import QtWidgets as QtW
 from PyQt5.QtCore import Qt, pyqtSignal
+import NodeGraphQt as NGQt
 
 
 disease_stages = [
@@ -29,38 +19,71 @@ disease_stages = [
 ]
 
 
-class DiseaseConfigWidget(QWidget):
+class StartNode(NGQt.BaseNode):
+    __identifier__ = "stages"
+    NODE_NAME = "Start State"
+
+    def __init__(self):
+        super(StartNode, self).__init__()
+        self.set_color(40, 150, 40)
+        self.add_output("Next Stage")
+
+
+class TransitionNode(NGQt.BaseNode):
+    __identifier__ = "stages"
+    NODE_NAME = "Transition"
+
+    def __init__(self):
+        super(TransitionNode, self).__init__()
+        self.set_color(220, 160, 20)
+        self.add_input("From Stage")
+        self.add_output("To Stage")
+
+        self.add_text_input("Prob", "Probability", text="0.1")
+
+
+class EndNode(NGQt.BaseNode):
+    __identifier__ = "stages"
+    NODE_NAME = "End State"
+
+    def __init__(self):
+        super(EndNode, self).__init__()
+        self.set_color(180, 40, 40)
+        self.add_input("From Stage")
+
+
+class DiseaseConfigWidget(QtW.QWidget):
     config_saved = pyqtSignal(dict)
 
     def __init__(self):
         super().__init__()
         self.setObjectName("configPanel")
-        layout = QVBoxLayout(self)
+        layout = QtW.QVBoxLayout(self)
 
         layout.setContentsMargins(20, 20, 20, 10)
 
-        title_label = QLabel("Disease Config")
+        title_label = QtW.QLabel("Disease Config")
         title_label.setObjectName("title_label")
         layout.addWidget(title_label)
 
-        layout.addWidget(QLabel("Name:"))
-        self.name_entry = QLineEdit()
+        layout.addWidget(QtW.QLabel("Name:"))
+        self.name_entry = QtW.QLineEdit()
         self.name_entry.setPlaceholderText("e.g., COVID-19 / Measles / Black Death")
         layout.addWidget(self.name_entry)
 
-        layout.addWidget(QLabel("Default Lowest Stage:"))
-        self.dls_combo = QComboBox()
+        layout.addWidget(QtW.QLabel("Default Lowest Stage:"))
+        self.dls_combo = QtW.QComboBox()
         self.dls_combo.addItems(disease_stages)
         self.dls_combo.setCurrentText(disease_stages[0])
         layout.addWidget(self.dls_combo)
 
-        layout.addWidget(QLabel("Max Mild Symptom Tag:"))
-        self.mmst_combo = QComboBox()
+        layout.addWidget(QtW.QLabel("Max Mild Symptom Tag:"))
+        self.mmst_combo = QtW.QComboBox()
         self.mmst_combo.addItems(disease_stages)
         self.mmst_combo.setCurrentText(disease_stages[0])
         layout.addWidget(self.mmst_combo)
 
-        self.save_button = QPushButton("Save Configuration")
+        self.save_button = QtW.QPushButton("Save Configuration")
         self.save_button.clicked.connect(self.getConfigData)
         layout.addWidget(self.save_button)
 
@@ -82,44 +105,81 @@ class DiseaseConfigWidget(QWidget):
         self.config_saved.emit(config_data)
 
 
-class NodeGraphWidget(QWidget):
+class NodeGraphWidget(QtW.QWidget):
     def __init__(self):
         super().__init__()
-        self.setStyleSheet("background-color:#d9d9d9")
 
-        layout = QVBoxLayout(self)
-        label = QLabel("placeholder")
-        layout.addWidget(label)
+        layout = QtW.QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.graph = NGQt.NodeGraph()
+
+        layout.addWidget(self.graph.viewer())
+
+        self.graph.register_nodes([StartNode, TransitionNode, EndNode])
+        self.graph.set_background_color(38, 50, 56)
+        self.graph.set_grid_color(55, 71, 79)
+
+        self._setup_context_menu()
+
+    def _setup_context_menu(self):
+        graph_menu = self.graph.get_context_menu("graph")
+        stage_menu = graph_menu.add_menu("stages")
+
+        func_start = lambda: self.graph.create_node(
+            StartNode.__identifier__ + "." + StartNode.__name__
+        )
+
+        func_transition = lambda: self.graph.create_node(
+            TransitionNode.__identifier__ + "." + TransitionNode.__name__
+        )
+
+        func_end = lambda: self.graph.create_node(
+            EndNode.__identifier__ + "." + EndNode.__name__
+        )
+
+        stage_menu.add_command(StartNode.NODE_NAME, func_start)
+        stage_menu.add_command(TransitionNode.NODE_NAME, func_transition)
+        stage_menu.add_command(EndNode.NODE_NAME, func_end)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QtW.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("pandemic-config-gui")
         self.resize(1280, 720)
 
-        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter = QtW.QSplitter(Qt.Horizontal)
 
         left_panel = DiseaseConfigWidget()
-        right_panel = NodeGraphWidget()
+        self.right_panel = NodeGraphWidget()
 
         left_panel.config_saved.connect(self.handle_config_save)
 
         self.splitter.addWidget(left_panel)
-        self.splitter.addWidget(right_panel)
+        self.splitter.addWidget(self.right_panel)
+
+        self.splitter.setSizes([300, 980])
+
         self.setCentralWidget(self.splitter)
 
     def handle_config_save(self, config_data):
         print("MainWindow received saved config.")
+
+        graph_data = self.right_panel.graph.serialize_session()
+        print("Current Graph Data:", graph_data)
         pass
 
 
 def run_app():
-    app = QApplication(sys.argv)
+    app = QtW.QApplication(sys.argv)
     window = MainWindow()
 
-    qss_path = os.path.join(os.path.dirname(__file__), "style", "theme.qss")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    qss_path = os.path.join(script_dir, "style", "theme.qss")
+
     if os.path.exists(qss_path):
         try:
             with open(qss_path, "r") as f:
